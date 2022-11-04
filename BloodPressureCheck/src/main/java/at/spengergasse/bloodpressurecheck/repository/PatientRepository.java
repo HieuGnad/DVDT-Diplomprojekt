f9d346835294f3db139fcf9a1da93b4fc74cf472
HieuGnad <97589125+HieuGnad@users.noreply.github.com>
2022-11-04 09:59:23 +0100

    Revert "Patient Commit 03.11.2022"
    This reverts commit fd1ac373c021c411ed2f96c676dbb19a9208b05e.

--- BloodPressureCheck/src/main/java/at/spengergasse/bloodpressurecheck/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package at.spengergasse.bloodpressurecheck.repository;
-import at.spengergasse.bloodpressurecheck.model.Patient;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PatientRepository extends
-        PagingAndSortingRepository<Patient, String> {
-}
